deadcf1fafc5d3bad1e6b974f92138f4a6683bbe
Ramit Roshann <ramitroshan777777@gmail.com>
2025-08-12 11:43:25 +0530

    update the code of .some() in object-loop

--- Object/object-loop.js
+++ Object/object-loop.js
@@ -1,7 +1,8 @@
-// some() and every() returns boolean- true/false
+//some() and every() returns boolean- true/false
 
 //some() -> one or more elements should satisfy the condition
-// const arr1 = {a:1, b:2, c:3};
+ 
+// const arr1 = [4, 101, 103];
 // //one or more even number
 // const result = arr1.some((ele) => {
 //     return ele %2 == 0;
@@ -9,21 +10,21 @@
 // console.log(result);
 
 
-// //every() -> every(all) elements should satisfy the conditions
-// const arr2 = [99, 101, 103];
-// // check every element is odd
-// const result2 = arr2.every((ele) => {
-//     return ele %2 != 0;
-// });
-// console.log(result2);
-
-
-
-const obj = {a:1, b:2, c:3};
-for(let key in obj){
-    console.log(`key - ${key} : value - ${obj[key]}`);
-}
-
-Object.keys([obj]).forEach((key) => {
-    console.log(key, obj[key]);
+//every() -> every(all) elements should satisfy the conditions
+const arr2 = [99, 101, 103];
+// check every element is odd
+const result2 = arr2.every((ele) => {
+    return ele %2 != 0;
 });
+console.log(result2);
+
+
+
+// const obj = {a:1, b:2, c:3};
+// for(let key in obj){
+//     console.log(`key - ${key} : value - ${obj[key]}`);
+// }
+
+// Object.keys([obj]).forEach((key) => {
+//     console.log(key, obj[key]);
+// });
