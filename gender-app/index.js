b75c6fefdd444ce4c9ca339527c5d18d60f079bc
Ramit Roshann <ramitroshan777777@gmail.com>
2025-08-23 12:32:02 +0530

    add folder gender-app

---
+++ gender-app/index.js
@@ -0,0 +1,6 @@
+//IQ) create a regisstration page where user will give name, email and in the process of typeing mail it should identify the gender ,automaticcaly
+
+
+
+
+console.log('index file');
