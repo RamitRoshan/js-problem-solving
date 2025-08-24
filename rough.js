cda4d8616205fefac31232118dbbc3dbc86631a3
Ramit Roshann <ramitroshan777777@gmail.com>
2025-08-24 22:09:52 +0530

    update rough.js

--- rough.js
+++ rough.js
@@ -1,16 +1,41 @@
-//practicing objects 
+//practicing Questions 
 
 
 /*
-I/P: 'dct'
-O/P: //{d:'D', c:'C', t: 'T'}
+Q1). Reverse a String in J.S
 */
-function pair(str){
-    const result = {};
-    for(let i=0; i<str.length; i++){
-        let char = str[i];
-        result[char] = char.toUpperCase();
-    }
-    return result;
+// const str = "java";
+// const reverse = str.split("").reverse().join("");
+// console.log(reverse);
+
+
+//Q2) Split the string
+// const str1 = "js, react, java";
+// const splitStr = str1.split(",");
+// console.log(splitStr);
+
+//Q3). Reverse a String without in-build methods
+// const str = "mulla";
+// let result = "";
+// for(let i= str.length-1; i>=0; i--){
+//     result = result + str[i];
+// }
+// console.log(result);
+
+
+//Q4). Remove Dublicates
+const str = "aabbcdeabec";
+let result = ""
+for(let i=0; i<str.length-1; i++){
+     let char = str[i];
+
+     if(!result.includes(char)){
+        result += char;
+     }
 }
-console.log(pair('dct')); 
+console.log(result);
+
+
+//Q5). 
+
+  
