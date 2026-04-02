959c58f7b7789645090a113a8cba2cfc9e01fcdd
Ramit Roshann <ramitroshan777777@gmail.com>
2026-04-02 23:25:29 +0530

    solved missing numbers

--- Arrays/Interv-Ques/missingNumber.js
+++ Arrays/Interv-Ques/missingNumber.js
@@ -5,9 +5,17 @@ Input: [1, 2, 4, 5] (N = 5)
 Output: 3
 */
 
-const arr = [1,2,4,5];
-let result = [];
+const arr = [1, 2, 4, 5];
 
+let n = 5;
 
+let expectedSum = n*(n+1)/2;
+
+let actualSum = 0;
 for(let i=0; i<arr.length; i++){
+    actualSum = actualSum + arr[i];
 }
+
+//missing 
+let missing = expectedSum - actualSum;
+console.log(missing);
