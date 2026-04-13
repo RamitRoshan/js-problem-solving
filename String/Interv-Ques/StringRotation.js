be18e2d45541f3e43c68570d81a439aa4b34a24e
Ramit Roshann <ramitroshan777777@gmail.com>
2026-04-13 19:52:52 +0530

    checking if string is a rotation of another string

---
+++ String/Interv-Ques/StringRotation.js
@@ -0,0 +1,22 @@
+/*
+Check if string is a rotation of another string
+
+Input:
+s1 = "abcd"
+s2 = "cdab"
+
+Output:
+true
+
+Input:
+s1 = "abcd"
+s2 = "acbd"
+
+Output:
+false
+*/
+
+const str1 = "abcd";
+const str2  = "cdab";
+
+
