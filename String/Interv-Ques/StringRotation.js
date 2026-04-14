ef8ff951a6dee6515a3ff2118b0be64845ebf877
Ramit Roshann <ramitroshan777777@gmail.com>
2026-04-14 17:19:59 +0530

    checking if string is a rotation of another string

--- String/Interv-Ques/StringRotation.js
+++ String/Interv-Ques/StringRotation.js
@@ -20,3 +20,21 @@ const str1 = "abcd";
 const str2  = "cdab";
 
 
+//edge cases
+if(str1.length !== str2.length){
+    return false;
+
+}else{
+    const result = (str1 + str1).includes(str2);
+    console.log(result);
+}
+
+// for(let i=0; i<str1.length; i++){
+    
+//     let last = str1[str1.length-1];
+//     str1 = last + str1.slice(0, s1.length - 1);
+// }
+
+
+
+
