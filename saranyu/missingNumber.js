eedaf0527f62060ff14d494dffad1e137515d457
Ramit Roshann <ramitroshan777777@gmail.com>
2026-05-09 23:02:43 +0530

    solved missing numbers

--- saranyu/missingNumber.js
+++ saranyu/missingNumber.js
@@ -9,5 +9,17 @@ Input: [1, 2, 3, 4, 6, 7, 8, ..., 100]
 Output: 5
 */
 
-const arr = [1, 2, 3, 4, 6, 7, 8, ...100];
-let result = ""
+const arr = [1, 2, 3, 4, 6, 7, 8,9,10];
+let n = 10;
+
+
+let expectedSum = n*(n+1)/2;
+
+let actualSum = 0;
+
+for(let i=0; i<arr.length; i++){
+    actualSum += arr[i];
+}
+
+let result = expectedSum - actualSum;
+console.log(result);
