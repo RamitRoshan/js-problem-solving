399ee2d0034e966569895600c04d6446a624b9b0
Ramit Roshann <ramitroshan777777@gmail.com>
2026-05-11 22:45:26 +0530

    solved countfreq

--- saranyu/countFrequency.js
+++ saranyu/countFrequency.js
@@ -14,3 +14,22 @@ p: 2
 l: 1  
 e: 1
 */
+
+const str = "apple";
+
+const result = {};
+
+for(let i=0; i<str.length; i++){
+    let ch = str[i];
+    if(result[ch]){
+        result[ch]++;
+    }else{
+        result[ch] = 1; //when the character is seen for the first time.
+    }
+}
+//console.log(result);
+
+for(let key in result){
+    console.log(key + ": " + result[key]);
+}
+ 
