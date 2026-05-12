cbe3888f7bb9e91f8196bbc9c69b53e9e853872e
Ramit Roshann <ramitroshan777777@gmail.com>
2026-05-12 22:33:50 +0530

    generated a random unique numbers btw 1 to 100

--- saranyu/UniqueRanNums.js
+++ saranyu/UniqueRanNums.js
@@ -7,3 +7,23 @@ Input: 100
 Output: [23, 1, 76, 54, 9, ..., 100]  (all numbers unique, order random)
 */
 
+
+const n = 100;
+let result = [];
+
+//fill array 1 to 100
+for(let i=1; i<=n; i++){
+    result.push(i);
+}
+
+//shuffle using one loop
+for(let i=n-1; i>0; i--){
+    let j = Math.floor(Math.random() * (i + 1));
+
+    //swap
+    let temp = result[i];
+    result[i] = result[j];
+    result[j] = temp;
+}
+
+console.log(result);
