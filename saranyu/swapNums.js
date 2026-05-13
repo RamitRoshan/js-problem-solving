5141f04b0e6f8c3e00726aa94289eaf0d8c06d13
Ramit Roshann <ramitroshan777777@gmail.com>
2026-05-13 22:26:52 +0530

    solved swap numbers using temp

---
+++ saranyu/swapNums.js
@@ -0,0 +1,34 @@
+/*
+Swap Two Variables (Without Temp)
+
+Question:
+Swap two numbers without using a third variable.
+
+Input:
+
+a = 5, b = 10
+
+Output:
+
+a = 10, b = 5
+*/
+
+
+
+//solved without using temp 
+let a = 5;
+let b = 10;
+
+
+
+
+//solved using temp 
+// let a = 5;
+// let b = 10;
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log("value of a after swap: " + a);
+// console.log("value of b after swap: " + b)
